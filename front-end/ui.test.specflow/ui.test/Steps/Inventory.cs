e8b38647a51366d65d19d1bb75d7b79c6f2f793d
bruno_hoffmann <bruno.hoffmann@cwi.com.br>
2021-08-25 21:58:46 -0300

    C:/Program Files/Git/

---
+++ front-end/ui.test.specflow/ui.test/Steps/Inventory.cs
@@ -0,0 +1,17 @@
+﻿using TechTalk.SpecFlow;
+using ui.test.Drivers;
+
+namespace ui.test.Steps
+{
+    [Binding]
+    public class Inventory : Browser
+    {
+
+        [Then(@"tenho o produto (.*) com o valor (.*)")]
+        public void EntaoTenhoOProdutoComOValor(string p0, string p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+    }
+}
